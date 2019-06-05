7e1473dc1fb14d5d203c6604614226239d77be7a
Erland Huamán <blackadress.01@gmail.com>
2019-06-05 11:17:30 -0500

    script generador de reportes de uso de sistema

--- server.sh
+++ server.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+FILE="$1"
+
+if [ -f "$FILE" ]
+then
+    echo "$FILE existe, generando logs"
+else
+    touch $FILE && echo "creando $FILE y generando logs"
+fi
+
 # Tiempo activo de servidor
-uptime | awk -F'( |,|:)+' '{printf("%s:%s ", ($6 * 24 + $8),  $9)}' >> log.csv
+uptime | awk -F'( |,|:)+' '{printf("%s:%s ", ($6 * 24 + $8),  $9)}' >> $FILE
 
 # Uso de CPU de servidor
-top -bn1 | grep Cpu | awk '{printf("%s ", $2)}' >> log.csv
+top -bn1 | grep Cpu | awk '{printf("%s ", $2)}' >> $FILE
 
 # Uso de RAM de servidor (Usado, Disponible, Total)
-free -h | awk 'NR ==2 { printf("%s %s %s ", $3, $7, $2) }' >> log.csv
+free -h | awk 'NR ==2 { printf("%s %s %s ", $3, $7, $2) }' >> $FILE
 
 # Numero de tareas
-top -bn1 | grep Task | awk '{ printf("%s\n", $2) }' >> log.csv
+top -bn1 | grep Task | awk '{ printf("%s\n", $2) }' >> $FILE
